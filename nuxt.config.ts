767ca1fa57f5c953ab1733b7dbab0cbfa27445fa
doi-soichiro <souichiro_doi@gio-tech.jp>
2025-05-12 17:10:18 +0900

    ソース保存時にインデント設定されるように修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
   modules: [
     '@nuxt/eslint',
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
-  ]
+  ],
+  devtools: { enabled: true },
+  compatibilityDate: '2024-11-01',
+  eslint: {
+    config: {
+      stylistic: {
+        indent: 2,
+        quotes: 'single',
+        semi: false,
+      },
+    },
+  },
 })
