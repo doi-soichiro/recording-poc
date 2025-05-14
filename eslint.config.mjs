91f802df90d610c700424f7c47a5fb9e0324298d
doi-soichiro <souichiro_doi@gio-tech.jp>
2025-05-14 12:23:59 +0900

    インデント修正

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,24 +2,24 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-    {
-        languageOptions: {
-            parserOptions: {
-                project: ['./tsconfig.json'], // TypeScriptプロジェクトの指定
-                tsconfigRootDir: process.cwd(),
-                sourceType: 'module',
-            },
-        },
-        rules: {
-            '@typescript-eslint/no-non-null-assertion': 'error',
-            '@typescript-eslint/no-unused-vars': 'error',
-            '@typescript-eslint/prefer-ts-expect-error': 'error',
-            '@typescript-eslint/no-explicit-any': 'error',
-            '@typescript-eslint/strict-boolean-expressions': 'warn',
-            'no-implicit-coercion': 'error',
-            'prefer-template': 'error',
-            'vue/multi-word-component-names': 'off',
-        },
-        ignores: ['eslint.config.mjs'], // 型情報が不要なファイルを無視
+  {
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.json'], // TypeScriptプロジェクトの指定
+        tsconfigRootDir: process.cwd(),
+        sourceType: 'module',
+      },
     },
+    rules: {
+      '@typescript-eslint/no-non-null-assertion': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/prefer-ts-expect-error': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/strict-boolean-expressions': 'warn',
+      'no-implicit-coercion': 'error',
+      'prefer-template': 'error',
+      'vue/multi-word-component-names': 'off',
+    },
+    ignores: ['eslint.config.mjs'], // 型情報が不要なファイルを無視
+  },
 )
