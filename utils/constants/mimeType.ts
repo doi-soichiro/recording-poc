cb8f4758bfb890d24b9fb015984d8756819ccaad
doi-soichiro <souichiro_doi@gio-tech.jp>
2025-05-14 12:07:20 +0900

    MIMEタイプの検証結果をメモ

--- utils/constants/mimeType.ts
+++ utils/constants/mimeType.ts
@@ -1,8 +1,6 @@
+// Edge、Chromeで利用可能なMIMEタイプは下記の通り
 //   WebmOpus = 'audio/webm;codecs=opus',
 //   Webm = 'audio/webm',
 //   WebmPcm = 'audio/webm;codecs=pcm',
 //   Mp4 = 'audio/mp4',
-//   ogg = 'audio/ogg' // 非対応、エラー
-//   mpeg = 'audio/mpeg' // 非対応、エラー
-//   aac = 'audio/aac' // 非対応、エラー
 export const RECORDED_DATA_MIME_TYPE = 'audio/webm;codecs=opus'
